d53c9974f8e98ab5d056288c498599ac9b350581
kanber28 <kanbermustafa10@gmail.com>
2023-04-05 11:31:38 +0300

    add mongo db connection instead of sqlite

--- Blog/settings.py
+++ Blog/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'blog',
+        'HOST': 'localhost',
+        'PORT': 27017,
+        'USER': '',
+        'PASSWORD': '',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from models import Post
 
-# Create your views here.
+
+def create_example_record(request):
+    data = Post(title='deneme Başlık', content='deneme uzun sayılabilecek kontent')
+    data.save()
